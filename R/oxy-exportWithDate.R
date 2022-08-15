91eb8631a2ad971061f1967dd24518d59139d7f7
aldisrauf <aldirauf@gmail.com>
2022-08-15 17:39:05 +0700

    edit again exportWithDate

--- R/oxy-exportWithDate.R
+++ R/oxy-exportWithDate.R
@@ -14,8 +14,8 @@
 #' @export 
 exportWithDate <- function(x, filename){
   
-  #rio::export(x, paste0(Sys.Date(), " ", filename))
-  print(5*6)
+  rio::export(x, paste0(Sys.Date(), " ", filename))
+  #print(5*6)
   
 }
 
